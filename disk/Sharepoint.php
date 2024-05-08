9a0d0fb7d9557fe1e0c76e21c6dbd260cf394fa3
wilyw520 <126368138+wilyw520@users.noreply.github.com>
2024-05-08 18:46:47 +0800

    Update Sharepoint.php

--- disk/Sharepoint.php
+++ disk/Sharepoint.php
@@ -5,7 +5,7 @@ class Sharepoint extends Onedrive {
 
     function __construct($tag) {
         $this->disktag = $tag;
-        $this->redirect_uri = 'https://scfonedrive.github.io';
+        $this->redirect_uri = 'https://redirectonedrive.github.io';
         if (getConfig('client_id', $tag) && getConfig('client_secret', $tag)) {
             $this->client_id = getConfig('client_id', $tag);
             $this->client_secret = getConfig('client_secret', $tag);
